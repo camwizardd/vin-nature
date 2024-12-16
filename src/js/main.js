78ce1f2813d275b1d3782393ec0536431469a8d6
camwizardd <camille.magnin@studentfr.ch>
2024-12-16 14:37:35 +0100

    installation d'un meilleur carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,12 @@
-const track = document.querySelector(".products");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const cards = document.querySelectorAll(".products .col-4");
+import Swiper, { Navigation, Pagination } from "swiper";
 
-let currentIndex = 0;
-const cardWidth = cards[0].offsetWidth + 24;
+Swiper.use([Navigation, Pagination]);
 
-function updateCarousel() {
-  track.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
-}
-
-// Boucle infinie
-function goToNext() {
-  if (currentIndex >= cards.length - 1) {
-    currentIndex = 0; // Retour au début
-    track.style.transition = "none";
-    updateCarousel();
-    setTimeout(() => {
-      track.style.transition = "transform 0.5s ease";
-      currentIndex++;
-      updateCarousel();
-    });
-  } else {
-    currentIndex++;
-    updateCarousel();
-  }
-}
-
-function goToPrev() {
-  if (currentIndex <= 0) {
-    currentIndex = cards.length - 1; // Retour à la fin
-    track.style.transition = "none";
-    updateCarousel();
-    setTimeout(() => {
-      track.style.transition = "transform 0.5s ease";
-      currentIndex--;
-      updateCarousel();
-    });
-  } else {
-    currentIndex--;
-    updateCarousel();
-  }
-}
-
-nextBtn.addEventListener("click", goToNext);
-prevBtn.addEventListener("click", goToPrev);
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
