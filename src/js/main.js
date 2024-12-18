da407de47c6dd08da044f3a9722d0c3081713e1a
camwizardd <camille.magnin@studentfr.ch>
2024-12-18 09:01:19 +0100

    ajout parallaxe index

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,39 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+document.addEventListener("scroll", function () {
+  const parallaxElements = document.querySelectorAll(".parallax");
+
+  parallaxElements.forEach((element) => {
+    // Récupérer la vitesse (data-speed)
+    const speed = parseFloat(element.getAttribute("data-speed"));
+    // Déplacer l'arrière-plan
+    const offset = window.scrollY * speed - 400;
+    element.style.backgroundPositionY = `${-offset}px`;
+  });
+});
+
+document.addEventListener("scroll", function () {
+  const parallaxElements = document.querySelectorAll(".parallax2");
+
+  parallaxElements.forEach((element) => {
+    // Récupérer la vitesse (data-speed)
+    const speed = parseFloat(element.getAttribute("data-speed"));
+    // Déplacer l'arrière-plan
+    const offset = window.scrollY * speed - 1000;
+    element.style.backgroundPositionY = `${-offset}px`;
+  });
+});
+
+document.addEventListener("scroll", function () {
+  const parallaxElements = document.querySelectorAll(".parallax3");
+
+  parallaxElements.forEach((element) => {
+    // Récupérer la vitesse (data-speed)
+    const speed = parseFloat(element.getAttribute("data-speed"));
+    // Déplacer l'arrière-plan
+    const offset = window.scrollY * speed - 1500;
+    element.style.backgroundPositionY = `${-offset}px`;
+  });
+});
